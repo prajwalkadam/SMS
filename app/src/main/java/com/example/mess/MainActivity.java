2b51aeef098fdd9eaf14c6eec5f3eb6b547841f3
Prajwal <prajwalkadam1172002@gmail.com>
2022-11-24 22:49:39 +0530

    not working now

--- app/src/main/java/com/example/mess/MainActivity.java
+++ app/src/main/java/com/example/mess/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -10,7 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText phonenumber,message;
+    EditText phonenumber;
     Button send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         send=findViewById(R.id.button);
         phonenumber=findViewById(R.id.editText);
-        message=findViewById(R.id.editText2);
+        //message=findViewById(R.id.editText2);
         send.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                String number=phonenumber.getText().toString();
-                String msg=message.getText().toString();
+                String number="+91"+phonenumber.getText().toString();
+                String msg="Help! I am in emergency. My Location : https://goo.gl/maps/yQdTrapGnQduBR4W7";
                 try {
                     SmsManager smsManager=SmsManager.getDefault();
-                    smsManager.sendTextMessage(number,null,msg,null,null);
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    PendingIntent pi= PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+                    smsManager.sendTextMessage(number,null,msg,pi,null);
                     Toast.makeText(getApplicationContext(),"Message Sent",Toast.LENGTH_LONG).show();
                 }catch (Exception e)
                 {
-                    Toast.makeText(getApplicationContext(),"Some fiedls is Empty",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Some fields is Empty",Toast.LENGTH_LONG).show();
                 }
             }
         });
